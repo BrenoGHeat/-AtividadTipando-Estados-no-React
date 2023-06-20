23a6a492dd229183bcb41eb8604f5e4138a00f17
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-06-20 17:24:25 -0300

    feat: post list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react"
+import { api } from "./services/api";
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      try {
+        setLoading(true);
+        const {data} = await api.get("/news");
+        setPostList(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+
+    }
+    loadPosts();
+  }, [])
+
+
   return (
     <div className="App">
-      
+      <ul>
+        {postList.map(post => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+            <span>{post.author}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
